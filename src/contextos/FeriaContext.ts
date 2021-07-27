4e7351478a69d87e12eb6c0f6587bf45fb629a24
Edgar Haro <nicolas.meli-at-912053765133>
2021-07-26 20:39:24 -0400

    context ok

--- src/contextos/FeriaContext.ts
+++ src/contextos/FeriaContext.ts
@@ -1,5 +1,5 @@
 import React from "react"
 
-const FeriaContext = React.createContext({handlerOnAdd:(objecto:any)=>{}, productos:[{}]})
+const FeriaContext = React.createContext({handlerOnAdd:(objecto:any)=>{}, productos: [{id:0,name:"",price:0}]})
 
 export default FeriaContext
